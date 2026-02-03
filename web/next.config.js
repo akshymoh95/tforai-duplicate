90fa9cb6c14ddcafe44441f894c9c6773181de72
akshymoh95 <akshaycrms@gmail.com>
2026-02-03 23:37:21 +0530

    Proxy /api to FastAPI inside container

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
+  },
+  async rewrites() {
+    return [
+      { source: "/api/:path*", destination: "http://127.0.0.1:8000/api/:path*" }
+    ];
   }
 };
 
